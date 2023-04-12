657781cf91abfcf1a56831cd45f204e34d844468
Preston Fitzgerald <prestonfitz@gmail.com>
2023-04-12 16:50:28 -0600

    Again with the data bases

--- PracticeWeb/settings.py
+++ PracticeWeb/settings.py
@@ -84,13 +84,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-    """'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'missing_persons',
-        'USER' : 'postgres',
-        'PASSWORD' : '4preston',
-        'HOST' : '0.0.0.0',
-    }"""
 }
 
 
