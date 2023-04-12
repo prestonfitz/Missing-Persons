5c037af0e0c7da7b6e185f0b2f98926810bae720
Preston Fitzgerald <prestonfitz@gmail.com>
2023-04-12 16:40:28 -0600

    Put it back

--- PracticeWeb/settings.py
+++ PracticeWeb/settings.py
@@ -80,6 +80,10 @@ WSGI_APPLICATION = 'PracticeWeb.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
     """'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'missing_persons',
