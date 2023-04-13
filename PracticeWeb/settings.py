3c2ebe2c13f0fa86cef41f6e45d6e4161cc4e4d2
Preston Fitzgerald <prestonfitz@gmail.com>
2023-04-12 18:25:41 -0600

    Statics

--- PracticeWeb/settings.py
+++ PracticeWeb/settings.py
@@ -125,7 +125,10 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'PracticeWeb/static')
+]      
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"  
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
